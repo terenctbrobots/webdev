0d7f111b5e0054b75d9037afeaaeabe0deb263f0
Terence Tan <terence@bigbadrobots.com>
2021-06-01 12:24:00 +0800

    Minor adjustments

--- gatsby/company/src/components/header.tsx
+++ gatsby/company/src/components/header.tsx
@@ -64,7 +64,7 @@ const Header = (props) => {
             <div className="flex items-center mx-40">
                 <img src={ClawImg} alt='claw'/>
                 <div className="flex justify-around items-center bg-nav-texture font-nav text-nav h-8 w-full">
-                    <div className={props.page === 'index' && 'text-white'}>
+                    <div className={props.page === 'home' && 'text-white'}>
                         <a className="flex justify-center items-center" href={props.page === 'index' ? '#' : '/'}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 lg:h-6 lg:w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
--- gatsby/company/src/pages/index.tsx
+++ gatsby/company/src/pages/index.tsx
@@ -16,41 +16,41 @@ const Home = () => {
         <Content>
             <div className="m-8">
                 <Topic>Information</Topic>
-                <p className="text-xl p-2">
+                <p className="md:text-xl p-2">
                 Big Bad Robots is software development company focused on Full-stack and Game development. We have experience with frameworks such as React, Django and backend database systems like PostgresSQL.
                 We have developed Games from concept to production. We provide either custom development or with Game Engines such as Unity/Godot on PC/MacOS/Console and Mobile.
                 </p >
-                <p className="text-xl p-2">
+                <p className="md:text-xl p-2">
                 Contact us if you looking for developers with over 20 years experience in Software and Game Development at biz -at- bigbadrobots.com
                 </p>
             </div>
             <div className="m-8">
                 <Topic>Latest</Topic>
-                <div className="mb-2 flex p-8 w-full h-30 bg-gray-800 text-white rounded-md">
+                <div className="mb-2 md:flex p-8 w-full h-30 bg-gray-800 text-white rounded-md">
                     <img  className="w-40 h-40 overflow-hidden" src={TailwindImg} alt='tailwind logo'/>
                     <div className="m-4">
-                        <p className="text-xl bold text-nav m-1">Website updated for mobile</p>
-                        <p className="text-lg italic m-1">1 June 2021</p>
+                        <p className="md:text-xl bold text-nav m-1">Website updated for mobile</p>
+                        <p className="md:text-lg italic m-1">1 June 2021</p>
                         <p className="m1">
                             We use the power of tailwind to make our site compatible with mobile and tablets.
                         </p>
                     </div>
                 </div>
-                <div className="mb-2 flex p-8 w-full h-30 bg-gray-800 text-white rounded-md">
+                <div className="mb-2 md:flex p-8 w-full h-30 bg-gray-800 text-white rounded-md">
                     <img  className="w-40 h-40 overflow-hidden" src={GatsbyImg} alt='gatsby logo'/>
                     <div className="m-4">
-                        <p className="text-xl bold text-nav m-1">Website updated!</p>
-                        <p className="text-lg italic m-1">27 May 2021</p>
+                        <p className="md:text-xl bold text-nav m-1">Website updated!</p>
+                        <p className="md:text-lg italic m-1">27 May 2021</p>
                         <p className="m1">
                         We updated our website to use the power of Gatsby!
                         </p>
                     </div>
                 </div>
-                <div className="mb-2 flex p-8 w-full h-30 bg-gray-800 text-white rounded-md">
+                <div className="mb-2 md:flex p-8 w-full h-30 bg-gray-800 text-white rounded-md">
                     <img  className="w-40 h-40 overflow-hidden" src={WebsiteImg} alt='website screenshot'/>
                     <div className="m-4">
-                        <p className="text-xl bold text-nav m-1">New Website!</p>
-                        <p className="text-lg italic m-1">12 May 2021</p>
+                        <p className="md:text-xl bold text-nav m-1">New Website!</p>
+                        <p className="md:text-lg italic m-1">12 May 2021</p>
                         <p className="m1">
                         We launch a new website that feels a bit retro but moves us forward with new products, services and workshops. Styled using Tailwind CSS no less!
                         </p>
